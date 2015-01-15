1a2a2ed28f01916fcc9cb928b29f85e3dd8b2f24
alin1337 <alin1337@users.noreply.github.com>
2015-01-15 22:25:57 +0200

    Update extras.h

--- extras.h
+++ extras.h
@@ -3,10 +3,13 @@
 #pragma once
 #include <iostream>
 
-#define STORE_ACCOUNT "./Data2.sky"
-
+#define STORE_ACCOUNT "./Data.sky"
+#define XOR_KEY 0x55
 
 bool FileExists(std::string path);
 bool FileDelete(std::string path);
-
+std::string capital(std::string name);
+std::string xorIT(std::string what);
+std::wstring s2ws(const std::string& str);
+std::string ws2s(const std::wstring& wstr);
 #endif
