d697705cb64b096e9eeb7b60f043da0fc6d4c6f4
alin1337 <alin1337@users.noreply.github.com>
2015-01-16 00:35:56 +0200

    Update extras.h

--- extras.h
+++ extras.h
@@ -2,9 +2,15 @@
 #define EXTRAS_H
 #pragma once
 #include <iostream>
+#include <vector>
+#include <afxwin.h>
+#include <codecvt>
+#include <fstream>
+#include <string>
+#include <sstream>
 
 #define STORE_ACCOUNT "./Data.sky"
-#define XOR_KEY 0x55
+#define XOR_KEY 0xFF
 
 bool FileExists(std::string path);
 bool FileDelete(std::string path);
@@ -12,4 +18,8 @@ std::string capital(std::string name);
 std::string xorIT(std::string what);
 std::wstring s2ws(const std::string& str);
 std::string ws2s(const std::wstring& wstr);
+bool WriteAccountData(std::string user, std::string password, bool createFlag);
+std::vector<std::string> ReadAccountData();
+std::vector<std::string> SplitString (std::string input);
+
 #endif
