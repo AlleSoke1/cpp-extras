7c0db529860a018390ec8996657995946dc68aec
alin1337 <alin1337@users.noreply.github.com>
2015-01-15 20:26:29 +0200

    Create extras.h

---
+++ extras.h
@@ -0,0 +1,12 @@
+#ifndef EXTRAS_H
+#define EXTRAS_H
+#pragma once
+#include <iostream>
+
+#define STORE_ACCOUNT "./Data2.sky"
+
+
+bool FileExists(std::string path);
+bool FileDelete(std::string path);
+
+#endif
