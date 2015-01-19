c76d1c8b843e830fada523cb999578416f88398d
alin1337 <alin1337@users.noreply.github.com>
2015-01-19 14:13:41 +0200

    Update extras.h

--- extras.h
+++ extras.h
@@ -1,14 +1,10 @@
 #ifndef EXTRAS_H
 #define EXTRAS_H
 #pragma once
-#include <iostream>
-#include <vector>
-#include <afxwin.h>
-#include <codecvt>
-#include <fstream>
-#include <string>
-#include <sstream>
 
+
+#define API_URL "http://reborn.dragonnest.ro/motto-yesno"
+#define USERAGENT "dragonnest.ro"
 #define STORE_ACCOUNT "./Data.sky"
 #define XOR_KEY 0xFF
 
@@ -21,5 +17,6 @@ std::string ws2s(const std::wstring& wstr);
 bool WriteAccountData(std::string user, std::string password, bool createFlag);
 std::vector<std::string> ReadAccountData();
 std::vector<std::string> SplitString (std::string input);
-
+std::vector<std::string> &SplitStringDelim(const std::string &s, char delim, std::vector<std::string> &elems);
+std::string ExePath();
 #endif
